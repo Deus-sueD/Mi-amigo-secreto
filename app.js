6f64f5cc14c335fe6b94c5a07084be07092a7942
Deus-sueD <diegooswaldofv@gmail.com>
2025-02-06 21:56:43 -0600

    Versión de prueba 1

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-let ListaAmigos = [];
+let listaAmigos = [];
+let resultado;
+let numeroMaximo = 10;
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo(){
+    let AmigoIngresado = document.getElementById('amigo').value;
+    
+    if (AmigoIngresado == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        //Cambiar bloque de programación por uno que cumpla los requerimientos
+        listaAmigos.push(AmigoIngresado);
+        numeroMaximo = listaAmigos.length;
+        console.log(AmigoIngresado)
+        console.log(listaAmigos);
+        //asignarTextoElemento('ul',`${listaAmigos}`);
+        document.getElementById('listaAmigos').value = listaAmigos;
+        limpiarCaja();
+    }
+    return;
+}
+
+function sortearAmigo(){
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo);
+    console.log(numeroGenerado);
+    resultado = listaAmigos[numeroGenerado];
+    asignarTextoElemento('ul',`Tu amigo secreto es: ${resultado}`);
+}
